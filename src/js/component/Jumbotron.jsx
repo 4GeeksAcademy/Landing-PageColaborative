8b7b5ff7156fcba888a4ee3fe2149dd9c49d81a0
rocady21 <68871996+rocady21@users.noreply.github.com>
2023-08-07 17:19:40 +0000

    commit

---
+++ src/js/component/Jumbotron.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Jumbotron() {
+    return (
+        // dentro de este div ira el codigo del Footer
+        <div>
+            
+        </div>
+    )
+}
+
+export default Jumbotron
